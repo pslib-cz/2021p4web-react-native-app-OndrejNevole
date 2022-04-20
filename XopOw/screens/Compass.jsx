290333d3eceb418dfaa5a842c4bb7ddc19530943
Ondřej Nevole <ondrej.nevole@pslib.cz>
2022-04-20 21:10:39 +0200

    iamloosingit

--- XopOw/screens/Compass.jsx
+++ XopOw/screens/Compass.jsx
@@ -1,22 +1,25 @@
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
-import { Magnetometer, MagnetometerUncalibrated } from 'expo-sensors';
 import React, { useState, useEffect } from 'react';
 import Angle from '../services/Compass';
 
 export const Compass = props => {
-  let angle = Angle();
-  const _degree = (angle) => {
-    return angle - 90 >= 0 ? angle - 90 : angle + 271;
-  };
-  let realAngle = _degree(angle);
+  let angle = Angle() - 90;
     return (
       <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Compass</Text>
-            <Text>{realAngle}</Text>
-            <Text style={{transform: [{ rotate: 360 - realAngle + 'deg' }]}}>{'>'}</Text>
-        </SafeAreaView>
+        <View style={styles.values}>
+            <Text>{'Compass'}</Text>
+            <Text>{angle}</Text>
+            <Text style={{transform: [{ rotate: (-angle - 90) + 'deg' }], fontSize: 100}}>{'>'}</Text>
+        </View>
+      </SafeAreaView>
     );
 }
+const styles = StyleSheet.create({
+  values: {
+      justifyContent: 'center',
+      alignItems: 'center',
+  }
+});
 
 export default Compass;
--- XopOw/screens/Home.jsx
+++ XopOw/screens/Home.jsx
@@ -1,22 +1,91 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
-import SafeAreaView from 'react-native-safe-area-view';
-import {Getlat, Getlon} from '../services/Location';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, Button  } from 'react-native';
 import Angle from '../services/Compass';
-import { transform } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { Getlat, Getlon } from '../services/Location';
+import SafeAreaView from 'react-native-safe-area-view';
 
 export const Home = props => {
-    let lat = Getlat();
-    let lon = Getlon();
-    let data = Angle().toString();
-    console.log(data);
+    const [dLat, setDLat] = useState(50.76526942546564);
+    const [dLon, setDLon] = useState(15.053161193749785);
+    const [lat, setLat] = useState(0);
+    const [lon, setLon] = useState(0);
+    //let lat = Getlat();
+    //let lon = Getlon();
+    let compass = Angle() - 90;
+    let _Angle = 0;
+
+    const locationReset = props => {
+        let dklat = Getlat();
+        let fklon = Getlon();
+        setLat(dklat);
+        setLon(fklon);
+    }
 
     return (
-        <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home!</Text>
-            <Text style={{transform: [{ rotate: data + 'deg' }]}}>{'>'}</Text>
+        <SafeAreaView>
+            <Text style={styles.h1}>set destination</Text>
+            <TextInput style={styles.input} onChangeText={setDLat} placeholder="latitude"></TextInput>
+            <TextInput style={styles.input} onChangeText={setDLon} placeholder="longitude"></TextInput>
+            <Button onPress={() => {
+                console.log('dLat: ' + dLat);
+                console.log('dLon: ' + dLon);
+                console.log('lat: ' + lat);
+                console.log('lon: ' + lon);
+                console.log('angle: ' + _Angle);
+                _Angle = LatLonToAngle(lat, lon, dLat, dLon);
+                console.log('angle: ' + _Angle);
+            }} title='confirm' ></Button>
+            <View style={styles.values}>
+                <Text>{'Angle'}</Text>
+                <Text style={{transform: [{ rotate: (-compass - _Angle - 90) + 'deg' }], fontSize: 100}}>{'>'}</Text>
+                <Text>{compass + _Angle}</Text>
+            </View>
+            <View style={styles.values}>
+                <Text>{lat}</Text>
+                <Text>{lon}</Text>
+                <Button title='reset position' onPress={() => {
+                    locationReset();
+                }} ></Button>
+            </View>
         </SafeAreaView>
     );
 }
+
+function LatLonToAngle(Slat, Slon, Dlat, Dlon) {
+    let DesLon = Dlon - Slon;
+    let y = Math.sin(DesLon) * Math.cos(Dlat);
+    let x = Math.cos(Slat) * Math.sin(Dlat) - Math.sin(Slat) * Math.cos(Dlat) * Math.cos(DesLon);
+    //console.log('y: ' + y);
+    //console.log('x: ' + x);
+    let brng = Math.atan2(y, x);
+
+    brng = brng * 180 / Math.PI;
+    brng = (brng + 360) % 360;
+    brng = 360 - brng;
+    //console.log('brng: ' + brng);
+    return brng;
+}
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        textAlign: 'center',
+    },
+    values: {
+        margin: 40,
+        padding: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    h1: {
+        height: 40,
+        margin: 12,
+        padding: 10,
+        textAlign: 'center',
+    },
+  });
 
 export default Home;
